fe63bcd2124d7b565394e9f4c4a011e9bf3c4061
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-07 16:14:42 +0530

    theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MdMenu } from "react-icons/md";
+import { CiLight } from "react-icons/ci";
+import { CiDark } from "react-icons/ci";
 
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
   return (
     <div className="max-w-7xl mx-auto p-6">
       <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-semibold">ABC School</h1>
+        <div className="flex items-center gap-3">
+          <h1 className="text-2xl font-semibold">ABC School</h1>
+          <div className="flex items-center gap-2 text-xl cursor-pointer">
+            <CiLight />
+            <CiDark />
+          </div>
+
+        </div>
         <div>
           <ul className="hidden sm:flex gap-6 font-medium">
             <li>
