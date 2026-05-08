7a52f2980ffeff90f4d37721cdfac9c3023a8435
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-08 15:46:53 +0530

    hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-white text-black dark:bg-black dark:text-white'>
+        <div className='max-w-7xl mx-auto px-6'>
+            <h1>Hero</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
