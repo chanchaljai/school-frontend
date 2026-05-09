0468d255212414eeafe2078d71a5f00976b43eed
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-09 07:54:36 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import img1 from "../assets/img1.jpg";
+import img2 from "../assets/img2.jpg";
+import img3 from "../assets/img3.jpg";
+
+const images = [img1, img2, img3];
 
 const Hero = () => {
-  return (
-    <div className='bg-white text-black dark:bg-black dark:text-white'>
-        <div className='max-w-7xl mx-auto px-6'>
-            <h1>Hero</h1>
-        </div>
-    </div>
-  )
-}
+  const [current, setCurrent] = useState(0);
 
-export default Hero
+  useEffect(() => {
+    const slider = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(slider);
+  }, []);
+
+  return (
+    <div className="relative h-[400px] overflow-hidden">
+      {images.map((img, index) => (
+        <img
+          key={index}
+          src={img}
+          alt=""
+          className={`absolute w-full h-full object-contain transition-opacity duration-700 ${
+            current === index ? "opacity-100" : "opacity-0"
+          }`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Hero;
